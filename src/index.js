8072382fb17db2cb56c0114fd44de55560eabed8
Gabriel <gabrielpereira050201@hotmail.com>
2021-07-13 17:09:30 -0300

    Added theme provider to change default theme

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 
 import Routes from './pages/Routes'
 
+import { ThemeProvider } from '@material-ui/core/styles'
+
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ThemeProvider>
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
