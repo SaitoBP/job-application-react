b691966cc2c458d5b2b6121516123f5fa213d387
Gabriel <gabrielpereira050201@hotmail.com>
2021-07-13 14:57:55 -0300

    Created a basic navbar-like component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Search</li>
+      <li>Notifications</li>
+      <li>Profile</li>
+    </ul>
+  )
+}
+
+export default Navbar
