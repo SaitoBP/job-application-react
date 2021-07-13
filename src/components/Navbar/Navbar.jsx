f84dcf0697b0c7159d89b25e87cf8a1d0e75872f
Gabriel <gabrielpereira050201@hotmail.com>
2021-07-13 15:59:07 -0300

    Added links on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <ul>
-      <li>Home</li>
-      <li>Search</li>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+
+      <li>
+        <Link to='/list'>List</Link>
+      </li>
+
       <li>Notifications</li>
       <li>Profile</li>
     </ul>
