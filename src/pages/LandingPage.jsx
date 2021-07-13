fc688a9cc2fe6ce1be9150ea0dda034d2b2bb3e8
Gabriel <gabrielpereira050201@hotmail.com>
2021-07-13 14:58:15 -0300

    Mockup for Landing Page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react'
+
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+import { Navbar } from '../components/Navbar'
 
 const LandingPage = () => {
-  return ( 
-    <h1>Hello</h1>
-   );
+  const handleSubmit = _e => {
+    _e.preventDefault()
+    console.log('teste')
+  }
+
+  return (
+    <Fragment>
+      <Container>
+        <Typography variant='h4'>Find a place that gives you the ultimate calm</Typography>
+
+        <form onSubmit={handleSubmit}>
+          <TextField label='Place' variant='outlined' type='text' />
+          <TextField label='Guests' variant='outlined' type='number' />
+          <TextField label='Date' variant='outlined' type='text' />
+          <TextField label='Nights' variant='outlined' type='number' />
+
+          <Button type='submit' variant='contained'>
+            Search Room
+          </Button>
+        </form>
+      </Container>
+
+      <Navbar />
+    </Fragment>
+  )
 }
- 
-export default LandingPage;
+
+export default LandingPage
