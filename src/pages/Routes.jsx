5f3e115822ed4276b592358c576bdc46384ea023
Gabriel <gabrielpereira050201@hotmail.com>
2021-07-13 15:54:35 -0300

    Added list route

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import LandingPage from './LandingPage'
+import List from './List'
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path='/'>
         <LandingPage />
+      </Route>
+
+      <Route path='/list'>
+        <List />
       </Route>
     </Switch>
   )
